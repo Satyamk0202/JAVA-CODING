da9fee3adf0275d40274621c047e276d401bf203
Satyam Kumar <satyakumar9667@gmail.com>
2025-12-23 23:25:14 +0530

    Add files via upload

---
+++ c6praticequestion.java
@@ -0,0 +1,69 @@
+public class c6praticequestion {
+    public static void main(String[]args){
+        // pratice problem 1
+//         int n=4;
+//         for(int i=n;i>0;i--){
+//             for(int j=0;j<i;j++){
+//  System.out.print("*");
+//             }
+//            System.out.print("\n");
+//         }
+//problem:-2
+// int sum=0;
+// int n=4;
+// for(int i=0;i<n;i++){
+//     sum=sum+(2*i);
+
+// }
+// System.out.println("the sum of first n natural number are:"+sum);
+
+// 
+// pratice problem 4
+// int n=10;
+// for(int i=10;i>=1;i--){
+//     System.out.println(n+"*"+i+"="+(n*i));
+// }
+//pratice problem5
+// int n=5;
+// for(int i=1;i<=5;i++){
+//     System.out.println(n+"*"+(n-i));
+// }
+//pratice problem 5
+// int n=5;
+// int i=1;
+// int factorial=1;
+// while(i<=n){
+//   factorial *= i;  
+//   i++;
+// }
+// System.out.println(factorial);
+//pratice problem 7
+// int n=4;
+// int i=1;
+// while(i<=n){
+//     int j=1;
+//     while(j<=i){
+// System.out.println("*");
+// j++;
+//     }
+//     System.out.println("\n");
+//     i++;
+
+// }
+// int n=8;
+// int sum=0;
+
+// for(int i=1;i<=10;i++){
+//     System.out.println(n+"*"+i+"="+(n*i));
+//     sum =sum+n*i;
+// }
+// System.out.println(sum);
+int sum=0;
+int n=4;
+for(int i=0;i<=4;i++){
+sum=sum+2*i;
+    System.out.println(sum);
+}
+    }
+    
+}
